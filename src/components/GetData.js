96fc6613c46c83c1da51716f9bf6782872bb515f
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-04 01:06:48 +0300

    npm i vega-lite vega-lite-api vega-tooltip vega commit

---
+++ src/components/GetData.js
@@ -0,0 +1,12 @@
+import {csv} from d3
+
+const csvUrl = 'https://gist.githubusercontent.com/DERRICKSONNDAMBUKI/3bb093ef910eda3218c313ee9cc5d72d/raw/sensei_auto_mpg_dataset'
+
+export const getData = async()=>{
+    const data = await  csv(csvUrl)
+
+    // have a look at the attributes available in the console
+    console.log(data[0]);
+
+    return data
+}
